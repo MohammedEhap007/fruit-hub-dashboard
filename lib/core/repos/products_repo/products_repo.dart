7e889e2f1463ea33ec20d0a335fd8113fb113f97
MohammedEhap007 <mohammedehap007@gmail.com>
2025-04-30 01:51:43 +0300

    create products repo

---
+++ lib/core/repos/products_repo/products_repo.dart
@@ -0,0 +1,5 @@
+import 'package:fruit_hub_dashboard/features/add_product/domain/entities/add_product_input_entity.dart';
+
+abstract class ProductsRepo {
+  Future<void> addProducts(AddProductInputEntity addProductInputEntity);
+}
---
+++ lib/core/repos/products_repo/products_repo_impl.dart
@@ -0,0 +1,11 @@
+import 'package:fruit_hub_dashboard/core/repos/products_repo/products_repo.dart';
+
+import '../../../features/add_product/domain/entities/add_product_input_entity.dart';
+
+class ProductsRepoImpl implements ProductsRepo {
+  @override
+  Future<void> addProducts(AddProductInputEntity addProductInputEntity) {
+    // TODO: implement addProducts
+    throw UnimplementedError();
+  }
+}
