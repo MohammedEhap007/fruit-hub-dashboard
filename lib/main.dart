5740c41d75ad55bd5063f981ce0cabcde2b29053
MohammedEhap007 <mohammedehap007@gmail.com>
2025-04-29 21:08:40 +0300

    configure dashboard with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:fruit_hub_dashboard/core/helper/on_generate_routes.dart';
 import 'package:fruit_hub_dashboard/features/dashboard/presentation/views/dashboard_view.dart';
 
-void main() {
+void main() async {
   runApp(const FruitHubDashboard());
+  await dotenv.load(fileName: ".env");
 }
 
 class FruitHubDashboard extends StatelessWidget {
