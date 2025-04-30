bd42a690c0318bffb52d84fa6f3c4b5fb6d7b895
MohammedEhap007 <mohammedehap007@gmail.com>
2025-05-01 00:08:36 +0300

    implement upload file in supabase storage service

--- lib/core/services/supabase_storage_service.dart
+++ lib/core/services/supabase_storage_service.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:fruit_hub_dashboard/core/services/storage_service.dart';
+import 'package:path/path.dart' as p;
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SupabaseStorageService implements StorageService {
@@ -16,7 +17,11 @@ class SupabaseStorageService implements StorageService {
 
   @override
   Future<String> uploadFile(File file, String path) {
-    // TODO: implement uploadFile
-    throw UnimplementedError();
+    String fileName = p.basename(file.path);
+    String fileExtention = p.extension(file.path);
+    var result = _supabase.client.storage
+        .from('fruits_images')
+        .upload('$path/$fileName.$fileExtention', file);
+    return result;
   }
 }
