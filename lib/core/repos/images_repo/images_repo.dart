afabedb4ef8792a5478f969ae62b8096706dab9d
MohammedEhap007 <mohammedehap007@gmail.com>
2025-05-01 00:50:06 +0300

    implement upload image in images repo

--- lib/core/repos/images_repo/images_repo.dart
+++ lib/core/repos/images_repo/images_repo.dart
@@ -4,5 +4,5 @@ import 'package:dartz/dartz.dart';
 import 'package:fruit_hub_dashboard/core/errors/failures.dart';
 
 abstract class ImagesRepo {
-  Future<Either<Failure, String>> uploadImage(File path);
+  Future<Either<Failure, String>> uploadImage(File image);
 }
